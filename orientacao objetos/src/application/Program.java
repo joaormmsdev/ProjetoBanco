f9b38ea3f36d5c7f3fb09cd2d2cd440122069919
João Renato Moreira Mendes da Silva <joaormms.dev@gmail.com>
2025-11-19 13:53:53 -0300

    funções e inicio projeto

--- orientacao objetos/src/application/Program.java
+++ orientacao objetos/src/application/Program.java
@@ -1,27 +1,28 @@
 package application;
 
+import entities.Triangle;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("Enter the measures of triangle x: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Enter the measures of triangle y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Area of triangle x: %.4f%n", areaX);
         System.out.printf("Area of triangle y: %.4f%n", areaY);
@@ -31,5 +32,8 @@ public class Program {
         } else {
             System.out.println("Larger area: triangle Y");
         }
+        sc.close();
+        
     }
+
 }
