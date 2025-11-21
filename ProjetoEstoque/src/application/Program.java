842594a242ff77d018e43875608b8c26e66aa8fd
João Renato Moreira Mendes da Silva <joaormms.dev@gmail.com>
2025-11-21 08:44:33 -0300

    exerciciosEstoque

--- ProjetoEstoque/src/application/Program.java
+++ ProjetoEstoque/src/application/Program.java
@@ -1,17 +1,36 @@
 package application;
 
-import entites.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class Program {
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         Product product = new Product();
-        System.out.println();
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
 
+        System.out.println("product data: " + product);
+        
+        System.out.println("enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println("enter the number of productos to be remove from stock: ");
+        quantity = sc. nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println("Updated data: " + product);
 
         sc.close();
     }
