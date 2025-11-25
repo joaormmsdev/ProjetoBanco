7a7851d61eee3ac3b2659ea5bb8c6d3515a1bb71
João Renato Moreira Mendes da Silva <joaormms.dev@gmail.com>
2025-11-25 07:58:38 -0300

    vetores

---
+++ Vetores/src/Main2.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i< vect.length; i++){
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc . nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0.0;
+
+        for (int i = 0; i < vect.length; i++){
+            sum += vect[i].getPrice();
+        }
+        double avg = sum/ vect.length;
+
+        System.out.println(avg);
+    }
+}
