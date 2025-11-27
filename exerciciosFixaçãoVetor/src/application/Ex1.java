855c852851e0d03921efeffdb6bb2d3d19796747
João Renato Moreira Mendes da Silva <joaormms.dev@gmail.com>
2025-11-27 08:24:39 -0300

    numero negativo

---
+++ exerciciosFixaçãoVetor/src/application/Ex1.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("quantos numero voce vai digitar? ");
+        int n = sc.nextInt();
+        
+        int[] vetor = new int[n];
+
+        for(int i =0; i< 0;i++){
+            System.out.println("Digite um numero");
+            vetor[i]= sc.nextInt();
+        }
+
+        for (int i = 0; i< 0; i++){
+            if ()
+        }
+
+
+    }
+}
